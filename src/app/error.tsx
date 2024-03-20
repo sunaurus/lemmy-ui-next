d556f87f35d834005f8ac5e99781590d3ad235b8
sunaurus <sander@saarend.com>
2024-03-20 20:19:57 +0200

    Remove random comment

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -13,10 +13,7 @@ export default function Error({
       <div>{JSON.stringify(error)}</div>
       <button
         className="mt-4 rounded-md bg-slate-500 px-4 py-2 text-sm text-white transition-colors hover:bg-slate-400"
-        onClick={
-          // Attempt to recover by trying to re-render the invoices route
-          () => reset()
-        }
+        onClick={() => reset()}
       >
         Take me back
       </button>
