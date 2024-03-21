b6fd7b372c6d5e59e8f8972135efac6a2e398190
sunaurus <sander@saarend.com>
2024-03-22 01:27:58 +0200

    Disable avif conversion for now

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {formats: ['image/avif', 'image/webp'], remotePatterns: [{protocol: 'https', hostname: '**'}]}
+    images: {
+        formats: [
+            // 'image/avif', //disabled because the conversion is seriously slow
+            'image/webp',
+        ],
+        remotePatterns: [{protocol: 'https', hostname: '**'}]
+    }
 };
 
 export default nextConfig;
